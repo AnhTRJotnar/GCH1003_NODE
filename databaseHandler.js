5b5aeece4b848dcc640bd651957127f2b23afd83
AnhTRJotnar <109334995+AnhTRJotnar@users.noreply.github.com>
2022-12-25 21:02:44 +0700

    hds

---
+++ databaseHandler.js
@@ -0,0 +1,41 @@
+const {MongoClient,ObjectId} = require('mongodb')
+const DATABASE_URL="mongodb+srv://AnhTGCH:Marielouise157@cluster0.989ywn7.mongodb.net/test"
+
+async function insertNewProduct(newProduct) {
+    let db = await getDB()
+    let id = await db.collection("products").insertOne(newProduct)
+    return id
+}
+async function getDB() {
+    let client = await mongoClient.connect(DATABASE_URL)
+    let db = client.db("GCH1002")
+    return db
+}
+
+async function getAllProducts() {
+    let db = await getDB()
+    let results = await db.collection("products").find().toArray()
+    return results
+}
+async function updateProduct(id, name, price, picUrl) {
+    let db = await getDB()
+    await db.collection("products").updateOne({ _id: ObjectId(id) },
+        { $set: { "name": name, "price": price, "picture": picUrl } })
+}
+async function findProductById(id) {
+    let db = await getDB()
+    const productToEdit = await db.collection('products').findOne({ _id: ObjectId(id) })
+    return productToEdit
+}
+async function deleteProductById(id) {
+    let db = await getDB()
+    await db.collection("products").deleteOne({ _id: ObjectId(id) })
+}
+async function findProductByName(nameSearch){
+    let db = await getDB()
+    const result = await db.collection("products").find({name: new RegExp(nameSearch, 'i')}).toArray()
+    console.log(nameSearch)
+    console.log(result)
+    return result;
+}
+module.exports = {findProductByName, insertNewProduct,getAllProducts,updateProduct,findProductById,deleteProductById }
