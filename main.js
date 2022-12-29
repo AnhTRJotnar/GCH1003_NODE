d41d974fb5332e6b97798cf60833e55ffb6dc4d7
AnhTRJotnar <109334995+AnhTRJotnar@users.noreply.github.com>
2022-12-29 12:16:32 +0700

    a

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+const { urlencoded } = require('express')
 var express = require('express')
 const { deleteProductById, getAllProducts, insertNewProduct, updateProduct, findProductById, findProductByName } = require('./databaseHandler')
 var app = express()
+var hbs = require('hbs')
 
+hbs.registerHelper('func', function(price){
+    if(price < 50){
+        return "red";
+    }else if (price >= 50 && price <= 70){
+        return"green";
+    }else{
+        return "yellow"
+    }
+})
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
@@ -22,14 +33,21 @@ app.post('/new',async (req,res)=>{
     const name = req.body.txtName
     const price = req.body.txtPrice
     const picUrl = req.body.txtPic
-    const newProduct = {
-        name :name,
-        price: Number.parseFloat(price),
-        picture: picUrl
-    }
-    await insertNewProduct(newProduct)
-    res.redirect('/all')
-
+    if (name.length < 2){
+        res.render('newProduct', {  nameError:'Name should be over 2 characters' })
+    }else if (picUrl.length==0){
+        res.render('newProduct', {  picError:'You have not uploaded a picture' })
+    }else if (isNaN(price) == true){
+        res.render('newProduct', {  priceError:'You have not uploaded a picture' })
+    }else{
+        const newProduct = {
+            name :name,
+            price: Number.parseFloat(price),
+            picture: picUrl
+        }
+        await insertNewProduct(newProduct)
+        res.redirect('/all')
+        }
 })
 
 app.post('/search', async (req, res)=>{
